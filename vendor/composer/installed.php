9f39c2009121b6865e6dc8cc50998062c8ed7b39
Hugo Kinahan <hkinahan@wundermancommerce.com>
2022-01-14 16:38:52 +0000

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.0',
+        'version' => '1.0.0.0',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'hkinahan/version2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hkinahan/version2' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
